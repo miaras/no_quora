9091b1e8a476abbbf0c83512205e87fdbbb1b5cb
aricha1940 <aricha1940@users.noreply.github.com>
2026-01-09 19:08:50 +0100

    Clean up content.js

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
 if (window.location.href.includes("google.com/search?")) {
-
     let u = window.location.href;
     let u_changed = false;
     const arr = u.split("&");
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i].includes("q=")
-            && !arr[i].includes("+-quora.com")) {
+        if (arr[i].includes("q=") && !arr[i].includes("+-quora.com")) {
             arr[i] = arr[i].concat("+-quora.com");
             u_changed = true;
         }
